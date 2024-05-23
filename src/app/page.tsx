b0d05aa361899fd9944bda792f79c58b0ea955fa
Tudor Comparan <tudor.comparan@gmail.com>
2024-05-22 18:26:05 -0600

    page import usersList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { users } from "@/mock/users";
+import { UsersList } from "@/components/UsersList";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex-col flex gap-4 justify-center items-center">
-      {users.map((user) => (
-        <p key={user.id}>{user.firstName}</p>
-      ))}
+      <UsersList />
     </div>
   );
 }
