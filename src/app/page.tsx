0377362b657535e0acd80816161d2eb1b6f803ff
Tudor Comparan <tudor.comparan@gmail.com>
2024-05-22 21:17:32 -0600

    move jsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,44 @@
-import { UsersList } from "@/components/UsersList";
+import { getBaseUrl } from "@/utils/getBaseUrl";
+import { User } from "@nextui-org/react";
+import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
-export default function Home() {
+async function getUsers(): Promise<{ data: User[] }> {
+  const res = await fetch(`${getBaseUrl()}/api/v0/users`);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const { data } = await getUsers();
   return (
     <div className="flex-col flex gap-4 justify-center items-center p-10">
-      <UsersList />
+      <div className="flex flex-col gap-6">
+        {data.map((user) => (
+          <div className="w-full max-w-[300px]" key={user.id}>
+            <User
+              name={`${user.firstName} ${user.lastName}`}
+              className="text-white "
+              description={
+                <Link className="text-primary" href={`/users/${user.username}`}>
+                  @{user.username}
+                </Link>
+              }
+              avatarProps={{
+                isBordered: true,
+                color: "primary",
+                size: "lg",
+                src: user.image,
+              }}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
