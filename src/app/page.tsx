7b62c97b7cecee848a7e0869baf48dc97098e3b2
Tudor Comparan <tudor.comparan@gmail.com>
2024-05-22 18:03:32 -0600

    render first list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { users } from "@/mock/users";
+
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <div className="min-h-screen flex-col flex gap-4 justify-center items-center">
+      {users.map((user) => (
+        <p key={user.id}>{user.firstName}</p>
+      ))}
+    </div>
+  );
 }
